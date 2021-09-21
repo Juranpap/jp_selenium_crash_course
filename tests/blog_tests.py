1d79b50578035df943687e4277e946a7daf0a85a
JuranPap <juran.papuga@infinum.com>
2021-09-21 13:21:30 +0200

    Added 1.2test

--- tests/blog_tests.py
+++ tests/blog_tests.py
@@ -2,6 +2,7 @@ from utilities.base_test import BaseTest
 from utilities.loggers import log_message
 from pages.blog_page import BlogPage
 
+import time
 
 class BlogTests(BaseTest):
     def setUp(self):
@@ -16,3 +17,13 @@ class BlogTests(BaseTest):
         self.blog_page.save_screenshot(self._testMethodName)
 
         self.assertTrue(self.blog_page.topic_label.text == "Shameless")
+
+    def test_navigate_to_technology_post(self):
+        log_message("Navigates to Technology blog posts and verifies that the topic label is Technology.")
+
+        self.blog_page.navigate_to_page()
+        self.blog_page.technology_link.click()
+
+        self.assertTrue(self.blog_page.topic_label.text == "Technology")
+
+        time.sleep(5)
